351b10ba3543d80b244246f05c94e59a641eb3f4
neisyyaa <neisyyaa23@gmail.com>
2023-03-01 19:37:25 +0700

    Tugas pertemuan_2

--- src/Pertemuan2/Tugas.java
+++ src/Pertemuan2/Tugas.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Pertemuan_2;
+package Pertemuan2;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
